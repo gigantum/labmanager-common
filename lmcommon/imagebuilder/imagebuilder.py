1b72a9662173ecb1555f6bce45cdb45a6f95995c
Bill Van Besien <bill@gigantum.io>
2017-08-07 13:03:33 -0400

    Refactored the ImageBuilder

--- lmcommon/imagebuilder/imagebuilder.py
+++ lmcommon/imagebuilder/imagebuilder.py
@@ -20,17 +20,58 @@
 
 import datetime
 import typing
+import yaml
 import os
 
 class ImageBuidler(object):
     """Class to ingest indexes describing base images, environments, and dependencies into Dockerfiles. """
 
-    def __init__(self, indexed_data: typing.Dict) -> None:
-        self.indexed_data = indexed_data
-        self._validate_index()
+    def __init__(self, labbook_directory: typing.AnyStr) -> None:
+        self.labbook_directory = labbook_directory
+        if not os.path.exists(self.labbook_directory):
+            raise IOError("Labbook directory {} does not exist.".format(self.labbook_directory))
+        self._validate_labbook_tree()
 
-    def _validate_index(self) -> None:
-        """Throw exception if self.indexed_data does not follow anticipated schema or is missing keys. """
+    def _validate_labbook_tree(self) -> None:
+        """Throw exception if labbook directory structure not in expected format. """
+        subdirs = [['.gigantum'],
+                   ['.gigantum', 'env'],
+                   ['.gigantum', 'env', 'base_image'],
+                   ['.gigantum', 'env', 'dev_env'],
+                   ['.gigantum', 'env', 'package_manager']]
+
+        for subdir in subdirs:
+            if not os.path.exists(os.path.join(self.labbook_directory, *subdir)):
+                raise ValueError("Labbook directory missing subdir `{}'".format(subdir))
+
+    def _load_baseimage(self) -> typing.List[typing.AnyStr]:
+        base_images = [f for f in os.listdir(self.labbook_directory, '.gigantum', 'env', 'base_image')
+                       if os.path.isfile(f)]
+
+        assert len(base_images) == 1
+
+        with open(base_images[0]) as base_image_file:
+            fields = yaml.load(base_image_file)
+
+        generation_ts =str(datetime.datetime.now())
+        docker_owner_ns = fields['image']['namespace']
+        docker_repo = fields['image']['repo']
+        docker_tag = fields['image']['tag']
+
+        docker_lines: typing.List[typing.AnyStr] = []
+        docker_lines.append("# Dockerfile generated on {}".format(generation_ts))
+        docker_lines.append("# Name: {}".format(fields["info"]["human_name"]))
+        docker_lines.append("# Description: {}".format(fields["info"]["description"]))
+        docker_lines.append("# Author: {} <{}>, {}".format(fields['author']['name'], fields['author']['email'],
+                                                           fields['author']['organization']))
+        docker_lines.append("")
+        docker_lines.append("FROM {}/{}:{}".format(docker_owner_ns, docker_repo, docker_tag))
+
+
+    def _load_devenv(self) -> typing.List[typing.AnyStr]:
+        pass
+
+    def _load_packages(self) -> typing.List[typing.AnyStr]:
         pass
 
     def assemble_dockerfile(self) -> typing.AnyStr:
@@ -40,18 +81,8 @@ class ImageBuidler(object):
             typing.AnyStr - Content of Dockerfile.
         """
 
-        fields = self.indexed_data["gig-dev_environment-components"]["gigantum"]["ubuntu1604-python3"]["0.1.0"]
-        generation_ts =str(datetime.datetime.now())
-
-        docker_owner_ns = fields['image']['namespace']
-        docker_repo = fields['image']['repo']
-        docker_tag = fields['image']['tag']
-
-        docker_lines: typing.List[typing.AnyStr] = []
-        docker_lines.append("# Dockerfile generated on {}".format(generation_ts))
-        docker_lines.append("# Name: {}".format(fields["info"]["human_name"]))
-        docker_lines.append("# Description: {}".format(fields["info"]["description"]))
-        docker_lines.append("")
-        docker_lines.append("FROM {}/{}:{}".format(docker_owner_ns, docker_repo, docker_tag))
-
+        baseimage_lines = self._load_baseimage()
+        devenv_lines = []
+        package_lines = []
+        docker_lines = baseimage_lines + devenv_lines + package_lines
         return os.linesep.join(docker_lines)
