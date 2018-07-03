15582d735eb78b2a389cd205aaecbffd2e6a4898
Bill Van Besien <bill@gigantum.io>
2018-07-03 15:14:26 -0400

    get low level api
    Signed-off-by: Bill Van Besien <bill@gigantum.io>

--- lmcommon/container/core.py
+++ lmcommon/container/core.py
@@ -104,8 +104,7 @@ def build_docker_image(root_dir: str, override_image_tag: Optional[str], nocache
         image_id = None
         # From: https://docker-py.readthedocs.io/en/stable/api.html#docker.api.build.BuildApiMixin.build
         # This builds the image and generates output status text.
-        base = os.environ.get('DOCKER_HOST')
-        for line in APIClient(base_url=base).build(path=env_dir, tag=image_name, pull=True, nocache=nocache, forcerm=True):
+        for line in docker.from_env().api.build(path=env_dir, tag=image_name, pull=True, nocache=nocache, forcerm=True):
             print(f"-------------------- {line}")
             ldict = json.loads(line)
             stream = (json.loads(line).get("stream") or "").strip()
