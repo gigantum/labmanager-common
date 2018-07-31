5972aab4e9571ecc3086e64adf768b5e372cfc6b
Bill Van Besien <bill@gigantum.io>
2018-07-31 12:29:21 -0400

    Added new test if we can reuse image
    Signed-off-by: Bill Van Besien <bill@gigantum.io>

--- lmcommon/container/core.py
+++ lmcommon/container/core.py
@@ -26,6 +26,7 @@ from typing import Callable, Optional, List, Tuple, Any, Dict
 from docker import APIClient
 
 from lmcommon.configuration import get_docker_client
+from lmcommon.configuration.utils import call_subprocess
 from lmcommon.logging import LMLogger
 from lmcommon.labbook import LabBook
 from lmcommon.container.utils import infer_docker_image_name
@@ -52,6 +53,45 @@ def get_container_ip(lb_key: str) -> str:
     client = get_docker_client()
     container = client.containers.get(lb_key)
     return container.attrs['NetworkSettings']['Networks']['bridge']['IPAddress']
+
+
+def _get_cached_image(env_dir: str, image_name: str) -> Optional[str]:
+    """
+    Get Docker image id for the given environment specification (if it exsits).
+
+    This helps to determine if we can avoid having to rebuild the Docker image
+    by hashing the environemnt specification and determine if it changed. Any
+    change in content or version will cause the checksum to be different,
+    necessitating a rebuild. If there's no change, however, we can avoid potentially
+    costly rebuilds of the image.
+
+    Args:
+        env_dir: Environment directoryt for a LabBook
+        image_name: Name of the LabBook Docker image
+
+    Returns:
+        docker image id (Optional)
+    """
+    # Determine if we need to rebuild by testing if the environment changed
+    env_cache_path = os.path.join('/mnt/gigantum', f"{image_name}.cache")
+    yamllist_cmds = f'find -s {env_dir} -name "*.yaml" -type f -exec {{}} \\;'.split()
+    yamllist_cmds.extend(['|', 'sort', '|', 'cksum'])
+    env_cksum = call_subprocess(yamllist_cmds, cwd=env_dir)
+
+    if os.path.exists(env_cache_path):
+        old_env_cksum = open(env_cache_path).read()
+    else:
+        with open(env_cache_path, 'w') as cfile:
+            cfile.write(env_cksum)
+        return None
+
+    if env_cksum == old_env_cksum:
+        try:
+            i = get_docker_client().images.get(name=image_name)
+            return i.id
+        except docker.errors.ImageNotFound:
+            pass
+    return None
 
 
 def build_docker_image(root_dir: str, override_image_tag: Optional[str], nocache: bool = False,
@@ -91,6 +131,13 @@ def build_docker_image(root_dir: str, override_image_tag: Optional[str], nocache
     image_name = override_image_tag or infer_docker_image_name(labbook_name=lb.name,
                                                                owner=lb.owner['username'],
                                                                username=username)
+
+    reuse_image_id = _get_cached_image(env_dir, image_name)
+    if reuse_image_id:
+        logger.warning(f"Reusing Docker image for {str(lb)}")
+        if feedback_callback:
+            feedback_callback(f"Using cached image {reuse_image_id}")
+        return reuse_image_id
 
     # We need to remove any images pertaining to this labbook before triggering a build.
     try:
